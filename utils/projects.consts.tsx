493189058c9fd3f9b4921fd8ce0b14ffc0cfbeb6
Filip Porebski <porebskifilip@wp.pl>
2024-04-26 12:52:41 +0200

    added projects

--- utils/projects.consts.tsx
+++ utils/projects.consts.tsx
@@ -17,4 +17,59 @@ export const PROJECTS: ProjectType[] = [
     ],
     techStack: ["NextJs", "TypeScript", "Three.js"],
   },
+  {
+    title: "Whiteboard online",
+    description:
+      "A collaborative whiteboard where you can draw and chat with your friends in realtime.",
+    image: "/projects/whiteboard_project.png",
+    projectLinks: [
+      {
+        href: "https://tutoring-app-djdisterenterpise.koyeb.app/",
+        name: "Demo",
+      },
+      {
+        href: "https://github.com/DjDister/tutoring-app-v2",
+        name: "Github",
+        icon: <GithubIcon />,
+      },
+    ],
+    techStack: ["React", "ExpressJs", "Socket.io", "TypeScript"],
+  },
+  {
+    title: "Algorithms visualizer",
+    description:
+      "An interactive web application that visualizes different sorting algorithms. Which is being extended whenever I find a free time.",
+    image: "/projects/algorithms_project.png",
+    projectLinks: [
+      {
+        href: "https://algorithms-view.vercel.app/sorting-algorithms/merge-sort",
+        name: "Demo",
+      },
+      {
+        href: "https://github.com/DjDister/Algorithms-View",
+        name: "Github",
+        icon: <GithubIcon />,
+      },
+    ],
+    techStack: ["React", "TypeScript"],
+  },
+  {
+    title: "Stock Market Dashboard",
+    description:
+      "Developed with friends a stock market app to play with stocks and see the performance of the portfolio.",
+    image: "/projects/stock_market_project.png",
+    projectLinks: [
+      {
+        href: "https://stock-market-famk.vercel.app/",
+        name: "Demo",
+      },
+      {
+        href: "https://github.com/DjDister/StockMarketFAMK",
+
+        name: "Github",
+        icon: <GithubIcon />,
+      },
+    ],
+    techStack: ["React", "TypeScript"],
+  },
 ];
